cc5166dbf64aab81b27d5a05392e6aee58624982
Dilip Kumar N <dilipkumarn@N-Interns-255-DilipKumar.local>
2025-12-31 09:56:20 +0530

    setup completed

---
+++ src/main/java/org/dilip/first/pos_backend/PosBackendApplication.java
@@ -0,0 +1,14 @@
+package org.dilip.first.pos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosBackendApplication.class, args);
+        System.out.println("App is running");
+    }
+
+}
---
+++ src/test/java/org/dilip/first/pos_backend/PosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dilip.first.pos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
